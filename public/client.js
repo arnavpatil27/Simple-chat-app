95fd7aea7bace2402ee3b074e8903072fbf1affa
arnavpatil27 <1aaparnav@gmail.com>
2023-11-20 15:18:59 +0530

    whatsapp style interface

--- public/client.js
+++ public/client.js
@@ -1,14 +1,29 @@
-// public/client.js
 const socket = io();
-
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const messages = document.getElementById('messages');
 
+const nameForm = document.getElementById('name-form');
+const chatDiv = document.querySelector('.chat-app');
+const nameInput = document.getElementById('username');
+const displayNameSpan = document.getElementById('display-name');
+
+nameForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const displayName = nameInput.value.trim();
+  if (displayName) {
+    nameForm.style.display = 'none';
+    chatDiv.style.display = 'block';
+    displayNameSpan.textContent = displayName;
+    socket.emit('user joined', displayName); // Emit an event for user joining
+  }
+});
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (input.value) {
-    socket.emit('chat message', input.value);
+    const message = `${displayNameSpan.textContent}: ${input.value}`;
+    socket.emit('chat message', message);
     input.value = '';
   }
 });
